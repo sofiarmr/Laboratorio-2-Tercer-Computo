9761ea71a4614b1c6105369ac575967c20cb8907
sofiarmr <91625699+sofiarmr@users.noreply.github.com>
2023-11-25 19:31:31 -0600

    Update views.py

--- Lab2TercerComputo/GestorApp/views.py
+++ Lab2TercerComputo/GestorApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .formularios.registerform import NewUserForm
+from .formularios.loginform import LoginForm
+from django.http import HttpResponseRedirect
+from .models import Productos, Proveedores
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def reg_user(request):
+    if request.method == "POST":
+        formulario = NewUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+        return HttpResponseRedirect("/")
+    else:
+        formulario = NewUserForm()
+        return render(request, "Reg_user.html", {"form": formulario})
+
+
+def index(request):
+    return render(request, 'index.html')
+
